4db0d87addf20ad1d33bdfd8a3749313eb85f084
Carlos Eduardo <cedut.dev@gmail.com>
2025-10-31 10:47:05 -0300

    ✨ feat: adiciona o model Task

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,4 +1,78 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Task {
+    private static int nextId = 1;
+
+    private final int id;
+    private LocalDate date;
+    private LocalTime time;
+    private String title;
+    private String description;
+    private boolean completed;
+    private boolean deleted;
+
+    public Task(String title, String description, LocalDate date, LocalTime time) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.id = nextId++;
+        this.completed = false;
+        this.deleted = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
