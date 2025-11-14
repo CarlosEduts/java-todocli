7823281eb8c95ff5ce159f7830c668be3b836d7e
Carlos Eduardo <cedut.dev@gmail.com>
2025-11-14 11:29:22 -0300

    ⚙️ build: configura a conexão com o banco de dados MySQL

--- src/main/java/com/todocli/config/ConnectionFactory.java
+++ src/main/java/com/todocli/config/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package com.todocli.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalTime;
+
 public class ConnectionFactory {
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/todocli";
+        String username = "root";
+        String password = "root";
+
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new IllegalStateException("[Config] " + LocalTime.now() + " - Erro ao se conectar com o banco de dados", e);
+        }
+    }
 }
---
+++ src/main/java/com/todocli/util/TaskTableCreator.java
@@ -0,0 +1,4 @@
+package com.todocli.util;
+
+public class TaskTableCreator {
+}
